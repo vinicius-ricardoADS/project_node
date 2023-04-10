8b9ec06ea10bab49fafd2ceaac79293bbe79f5ee
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-04-10 15:08:38 -0300

    feat(app): back-end complete

--- src/app/middlewares/validarDados.js
+++ src/app/middlewares/validarDados.js
@@ -1,29 +1,29 @@
 function validarDadosAdmin(req) {
     if (req.path == "/") {
-        if (req.body.usuario !== null && req.body.usuario !== "" && req.body.senha !== null && req.body.senha !== "") 
+        if (req.body.usuario != null && req.body.usuario != "" && req.body.senha != null && req.body.senha != "") 
             return true;
         return false;
     }
 }
 
 function validarDadosPaciente (req) {
-    if (req.body.nome !== null && req.body.nome !== "" && req.body.cpf !== null && req.body.cpf !== "" 
-    && req.body.sexo !== null && req.body.sexo !== "" && req.body.datanasc !== null & req.body.datanasc !== "")
+    if (req.body.nome != null && req.body.nome != "" && req.body.cpf != null && req.body.cpf != "" 
+    && req.body.sexo != null && req.body.sexo != "" && req.body.datanasc != null & req.body.datanasc != "")
         return true;
     return false;
 }
 
 function validarDadosMedico (req) {
-    if (req.body.nome !== null && req.body.nome !== "" && req.body.crm !== null && req.body.crm !== "" && req.body.sexo !== null 
-        && req.body.sexo !== "" && req.body.datanasc !== null & req.body.datanasc !== "" && req.body.salario !== null && 
-        req.body.salario !== "")
+    if (req.body.nome != null && req.body.nome != "" && req.body.crm != null && req.body.crm != "" && req.body.sexo != null 
+        && req.body.sexo != "" && req.body.datanasc != null & req.body.datanasc != "" && req.body.salario != null && 
+        req.body.salario != "")
         return true;
     return false;
 }
 
 function validarDadosConsulta (req) {
-    if (req.body.data !== null && req.body.data !== "" && req.body.hora !== null && req.body.hora !== "" 
-    && req.body.idPaciente !== null && req.body.idPaciente !== "" && req.body.idMedico !== null && req.body.idMedico !== "")
+    if (req.body.data != null && req.body.data != "" && req.body.hora != null && req.body.hora != "" 
+    && req.body.idPaciente != null && req.body.idPaciente != "" && req.body.idMedico != null && req.body.idMedico != "")
         return true;
     return false;
 }
